25df735b746e74b0c40a5a2f384837aadd9e9d13
NataliaZanegina <nataliazanegina@yandex.ru>
2022-09-21 13:21:03 +0300

    Homework 5 is completed

---
+++ HelloPython/Homework5.py
@@ -0,0 +1,141 @@
+from random import randint
+# Задача 2. Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход 
+# определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются 
+# сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего 
+# конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+# def Users_steps():
+#     global count
+#     step = int(input('Сколько конфет Вы забираете? '))
+#     count -= step
+#     if count <= 0:
+#         print("Вы выиграли!")
+#     else:
+#         print('Осталось {} конфет'.format(count))
+        
+
+# def Bots_steps():
+#     global count, max_step
+#     if count % (max_step + 1) !=0:
+#         step = count % (max_step + 1)
+#     else:
+#         step = randint(1, 28)
+#     print('Бот забрал {} конфет'.format(step))
+#     count -= step
+#     if count <= 0:
+#         print('Вы проиграли!')
+#     else:
+#         print('Осталось {} конфет'.format(count))
+
+# count = 2021
+# max_step = 28
+
+# input('Чтобы бросить жребий, нажмите Enter: ')
+# lot = randint(0,1)
+
+# if lot:
+#     print('Первым ходите Вы.')
+# else:
+#     print('Первым ходит бот.')
+
+# while count > 0:
+#     if lot:
+#         Users_steps()
+#         if count > 0:
+#             Bots_steps()
+#     else:
+#         Bots_steps()
+#         if count > 0:
+#             Users_steps()
+
+
+# Задача 3. Создайте программу для игры в ""Крестики-нолики"".
+
+# def Show_Matrix(m):
+#     print()
+#     for i in range(len(m)):
+#         for j in range(len(m[i])):
+#             print(m[i][j], end=' ')
+#         print()
+#     print()
+
+# def Steps(sign, player):
+#     global matrix
+#     print('Ход игрока за {}.'.format(sign))
+#     step_row = int(input('Введите номер строки: '))
+#     step_column = int(input('Введите номер столбца: '))
+#     while matrix[step_row][step_column] != '*':
+#         print('Ошибка! Эта позиция уже занята.')
+#         step_row = int(input('Введите номер строки: '))
+#         step_column = int(input('Введите номер столбца: '))
+        
+#     matrix[step_row][step_column] = sign
+
+#     if step_row == 0:
+#         player.add(step_column)
+#     elif step_row == 1:
+#         player.add(step_column + len(matrix))
+#     else:
+#         player.add(step_column + 2*len(matrix))
+    
+#     Show_Matrix(matrix)
+#     if player in winning_combinations:
+#         print('Победа игрока за {}!'.format(sign))
+
+# matrix = [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']]
+# Show_Matrix(matrix)
+
+# winning_combinations = [{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}]
+
+# sign_x = 'X'
+# sign_0 = '0'
+# player_X = set()
+# player_0 = set()
+
+
+# while True:
+#     Steps(sign_x, player_X)
+#     if player_X in winning_combinations:
+#         break
+#     else:
+#         Steps(sign_0, player_0)
+#         if player_0 in winning_combinations:
+#             break
+
+
+# Задача 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные
+# хранятся в отдельных текстовых файлах.
+
+# task = str()
+
+# path = 'data_task4.txt'
+# data = open(path, 'r')
+# for line in data:
+#     task = line
+# data.close()
+
+
+# result = str()
+# count = 1
+
+# if len(task) == 1:
+#     result += task[0] + str(len(task))
+# elif len(task) != 0:
+#     temp = task[0]
+#     for i in range(1, len(task)):
+#         if task[i] == temp:
+#             count +=1
+#         else:
+#             result += task[i-1] + str(count)
+#             temp = task[i]
+#             count = 1
+#     result += task[i] + str(count)
+
+# with open('result_task4.txt', 'w') as data:
+#     data.write(result)
+
+
