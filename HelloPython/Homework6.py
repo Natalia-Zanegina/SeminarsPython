c164488a90320b66c35793c7f1524157234e2c39
NataliaZanegina <nataliazanegina@yandex.ru>
2022-09-25 00:18:52 +0300

    Homework 6 updated

--- HelloPython/Homework6.py
+++ HelloPython/Homework6.py
@@ -38,7 +38,7 @@ from random import randint
 # def element_for_index(num):
 #     global items
 #     return items[num]
-# elements_to_find = list(map(element_for_index, filter(lambda x: x%2 == 1, range(len(items)))))
+# elements_to_find = list(map(element_for_index, filter(lambda x: x%2, range(len(items)))))
 # result = sum(elements_to_find)
 # print(result)
 
